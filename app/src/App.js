0cba7f9dac7ee53cc8852d70e3c9b0c7dce194a0
Jorge Andrade <jorandradefig@gmail.com>
2018-03-09 12:42:59 -0600

    Update app component

--- app/src/App.js
+++ app/src/App.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Home from './Home'
-import Footer from './Footer'
-import Faq from './Faq'
-import Participa from './Participa'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
-        <Participa />
-        <Faq />
-        <Footer />
       </div>
      
     )
