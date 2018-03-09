cd2a253cd61b93faa38588919fd39cb30d99b8c5
Jorge Andrade <jorandradefig@gmail.com>
2018-03-09 13:16:33 -0600

    Update header

--- app/src/Home/Header.js
+++ app/src/Home/Header.js
@@ -7,6 +7,9 @@ import {
 import recurso1 from '../assets/Recurso1@10x.svg'
 import reto from '../assets/_RETO_.svg'
 import hidrocarburos from '../assets/CORRUPCION_EN_EL_SECTOR_DE_HIDROCARBUROS.svg'
+import embajada from '../assets/logo-embajada-mx.svg'
+import ethos from '../assets/ethos.svg'
+import cmx from '../assets/cmx_logo.svg'
 
 class Header extends Component {
   render() {
@@ -14,7 +17,7 @@ class Header extends Component {
       <div className="Header">
         <Container className="py-5">
           <Row>
-            <Col xs="12" lg="6">
+            <Col xs="12" lg="6" className="d-flex justify-content-center align-items-center">
               <img className="img-fluid" src={recurso1} />
             </Col>
             <Col xs="12" lg="6" className="d-flex flex-column justify-content-center align-items-center">
@@ -22,7 +25,12 @@ class Header extends Component {
               <img className="mt-3 img-fluid" src={hidrocarburos} />
             </Col>
           </Row>
-          <Row>
+          <Row className="d-flex justify-content-center align-items-center">
+            <Col xs="12" lg="6" className="pt-5 d-flex flex-row justify-content-between align-items-center">
+              <img className="img-fluid" src={embajada} />
+              <img className="img-fluid" src={ethos} />
+              <img className="img-fluid" src={cmx} />
+            </Col>
           </Row>
         </Container>
       </div>
