e8fd11deb023ad1fc7e1d99b5a81047eec1e98de
Jorge Andrade <jorandradefig@gmail.com>
2018-03-09 00:22:25 -0600

    Fix navbar

--- app/src/Home/index.js
+++ app/src/Home/index.js
@@ -1,51 +1,68 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem } from 'reactstrap'
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap'
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
   render() {
     return (
       <div className="Home">
         <Container fluid>
-            <Navbar color="faded" light expand="md">
-            <NavbarBrand href="/">Ethos</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                <NavItem>
-                    <NavLink href="">Hackatón</NavLink>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                    Options
-                    </DropdownToggle>
-                    <DropdownMenu >
-                    <DropdownItem>
-                        Option 1
-                    </DropdownItem>
-                    <DropdownItem>
-                        Option 2
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                        Reset
-                    </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                </Nav>
-            </Collapse>
-            </Navbar>
+          <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">Ethos</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="">Hackatón</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+              Options
+              </DropdownToggle>
+              <DropdownMenu >
+              <DropdownItem>
+                  Option 1
+              </DropdownItem>
+              <DropdownItem>
+                  Option 2
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                  Reset
+              </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+          </Navbar>
         </Container>
       </div>
     )
