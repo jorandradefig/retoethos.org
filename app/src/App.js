7168d8d2d2a4eccb066a4d3d9432668eed55b261
Jorge Andrade <jorandradefig@gmail.com>
2018-03-09 00:08:56 -0600

    Add home component

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import Home from './Home'
 
 class App extends Component {
   render() {
---
+++ app/src/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+      </div>
+    )
+  }
+}
+
+export default Home
