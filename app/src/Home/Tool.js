5710519019bcbeadf752541b7701443bb41f10ce
Jorge Andrade <jorandradefig@gmail.com>
2018-03-09 23:49:17 -0600

    Update features style

--- app/src/Home/Tool.js
+++ app/src/Home/Tool.js
@@ -16,33 +16,30 @@ class Tool extends Component {
   render() {
     return (
       <div className="Tool py-5">
-        <Container className="d-flex justify-content-center align-items-center px-5">
-          <Row>
-            <Col xs="12" lg="4" className="d-flex justify-content-center align-items-center px-5">
-                <img className="img-fluid" src={recurso2} />
+        <Container className="d-flex flex-column justify-content-center align-items-center px-5">
+          <Row className="px-0 px-lg-5">
+            <Col xs="12" lg="4" className="d-flex justify-content-center align-items-center">
+              <img className="img-fluid" src={recurso2} />
             </Col>
             <Col xs="12" lg="8" className="d-flex justify-content-center align-items-center mt-5 mt-lg-0">
-                <p className="text-center idea">Idear una herramienta que permita detectar posibles irregularidades y malas prácticas administrativas a partir del análisis de la información obtenida de bases de datos abiertos dentro del sector energético.</p>
+              <p className="text-center idea">Idear una herramienta que permita detectar posibles irregularidades y malas prácticas administrativas a partir del análisis de la información obtenida de bases de datos abiertos dentro del sector energético.</p>
+            </Col>
+          </Row>
+          <Row className="px-0 px-lg-5 mt-5">
+            <Col xs="col-md-12 " lg="col-md-12" className="px-0 px-lg-5 features">
+              <p className="mb-4">La herramienta deberá:</p>
+              <p>
+                <span className="greater"> &gt; </span> Identificar las áreas de riesgo más susceptibles a la corrupción en el sector.
+              </p>
+              <p>
+                <span className="greater"> &gt; </span> Generar conocimiento útil sobre actos de corrupción en el sector, mediante el análisis de datos abiertos.
+              </p>
+              <p>
+                <span className="greater"> &gt; </span> Integrar un sistema de alertas con el objetivo de facilitar la detección de posibles irregularidades o malas prácticas.
+              </p>
             </Col>
           </Row>
         </Container>
-        <Container className=" d-flex justify-content-center align-items-center">
-            
-            <Row>
-              <Col xs="col-md-12 " lg="col-md-12">
-                  <img className="Puntos align-self-center" src={recurso4} />
-              </Col>
-            </Row>
-        </Container>
-        <Container className="Fecha d-flex justify-content-center align-items-center">
-            
-            <Row>
-              <Col  xs="col-md-12 " lg="col-md-12">
-                  <img className="align-self-center" src={recurso5} />
-              </Col>
-            </Row>
-        </Container>
-        
       </div>
     )
   }
