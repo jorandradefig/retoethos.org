2e7f23c3ec3e54ae95632a80520481ea852f6568
Jorge Andrade <jorandradefig@gmail.com>
2018-03-13 00:17:08 -0600

    Add social icons

--- app/src/Home/Footer.js
+++ app/src/Home/Footer.js
@@ -14,6 +14,9 @@ import {
   DropdownItem
 } from 'reactstrap'
 import { Link } from 'react-scroll'
+import fontawesome from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter } from '@fortawesome/fontawesome-free-brands'
 
 class Footer extends Component {
   constructor(props) {
@@ -38,30 +41,38 @@ class Footer extends Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="" navbar>
-            <NavItem>
-              <NavLink className="mr-0 mr-lg-5" href="https://docs.google.com/document/d/1by8jK4GXsJL0lCcvc28BelhS0BtH6I0ImselfZ7A-PI/edit" target="_blank">BASES</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar direction="up">
-                <DropdownToggle className="mr-0 mr-lg-5" nav caret>
-                  RECURSOS
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem href="https://drive.google.com/open?id=1evJgsY8axXOpWujbDPgm2n5TZhMTc2ae" target="_blank">
-                    DATASETS
-                  </DropdownItem>
-                  <DropdownItem href="https://drive.google.com/open?id=1_ffcGjtcoSqPmLuJGnfOJCzRoeH_1sNp" target="_blank">
-                    CLASIFICACIÓN DE DATASETS
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            <NavItem>
-              <NavLink>
-                <Link to="faq" spy={true} smooth={true} duration={500}>
-                  FAQ
-                </Link>
-              </NavLink>
-            </NavItem>
-            </Nav> 
+              <NavItem>
+                <NavLink className="mr-0 mr-lg-5" href="https://docs.google.com/document/d/1by8jK4GXsJL0lCcvc28BelhS0BtH6I0ImselfZ7A-PI/edit" target="_blank">BASES</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar direction="up">
+                  <DropdownToggle className="mr-0 mr-lg-5" nav caret>
+                    RECURSOS
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem href="https://drive.google.com/open?id=1evJgsY8axXOpWujbDPgm2n5TZhMTc2ae" target="_blank">
+                      DATASETS
+                    </DropdownItem>
+                    <DropdownItem href="https://drive.google.com/open?id=1_ffcGjtcoSqPmLuJGnfOJCzRoeH_1sNp" target="_blank">
+                      CLASIFICACIÓN DE DATASETS
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              <NavItem>
+                <NavLink>
+                  <Link to="faq" spy={true} smooth={true} duration={500}>
+                    FAQ
+                  </Link>
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="mr-4">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </NavItem>
+              <NavItem className="mr-4">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </NavItem>
+            </Nav>
           </Collapse>
         </Navbar>
       </div>
